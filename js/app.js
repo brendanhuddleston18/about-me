f4d5df91207322b11bdbfdc892c083eca488a030
Brendan Huddleston <brendanhuddleston18@gmail.com>
2023-10-24 16:32:10 -0500

    Added welcome prompt to js, first two questions

---
+++ js/app.js
@@ -0,0 +1,23 @@
+let userName = prompt("Hey what's your name?");
+console.log(userName);
+alert(`Welcome ${userName}, to my work in progress 'About Me' site!`);
+
+let userAccept = prompt("We're gonna take a quick quiz is that okay? (Y/N)");
+
+if (userAccept.toLowerCase() === "y") {
+  console.log('Strap in here we go!');
+  alert('Strap in here we go!');
+} else {
+  while (userAccept.toLowerCase() !== "y") {
+    userAccept = prompt(
+      'You gotta agree to the test in order to view my site! So (Y/N)'
+    );
+  }
+}
+
+let brendanAge = prompt("How old am I?");
+
+switch(brendanAge) {
+  case: 25
+  alert('You got it!')
+}
