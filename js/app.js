eb5a7a6a1cddc2ce7ce0af5850a6e48ad3bacf68
Brendan Huddleston <brendanhuddleston18@gmail.com>
2023-10-24 16:36:24 -0500

    added switch with brendanAge variable

--- js/app.js
+++ js/app.js
@@ -5,19 +5,28 @@ alert(`Welcome ${userName}, to my work in progress 'About Me' site!`);
 let userAccept = prompt("We're gonna take a quick quiz is that okay? (Y/N)");
 
 if (userAccept.toLowerCase() === "y") {
-  console.log('Strap in here we go!');
-  alert('Strap in here we go!');
+  console.log("Strap in here we go!");
+  alert("Strap in here we go!");
 } else {
   while (userAccept.toLowerCase() !== "y") {
     userAccept = prompt(
-      'You gotta agree to the test in order to view my site! So (Y/N)'
+      "You gotta agree to the test in order to view my site! So (Y/N)"
     );
   }
 }
 
 let brendanAge = prompt("How old am I?");
 
-switch(brendanAge) {
-  case: 25
-  alert('You got it!')
+switch (brendanAge) {
+  case 25:
+    alert("You got it!");
+    break;
+  case 24:
+    alert("Almost had it!");
+    break;
+  case 25:
+    alert("Ooooo you were close!");
+    break;
+  default:
+    alert("Nope you are wrong");
 }
