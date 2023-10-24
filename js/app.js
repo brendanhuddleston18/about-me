684721b9ec85488e5bd9a6e987cb054cce200255
Brendan Huddleston <brendanhuddleston18@gmail.com>
2023-10-24 17:16:03 -0500

    added 5 (y/n) questions for lab 02

--- js/app.js
+++ js/app.js
@@ -1,7 +1,9 @@
+// Welcome Message w/ prompt
 let userName = prompt("Hey what's your name?");
 console.log(userName);
 alert(`Welcome ${userName}, to my work in progress 'About Me' site!`);
 
+//Question 1 (If condition)
 let userAccept = prompt("We're gonna take a quick quiz is that okay? (Y/N)");
 
 if (userAccept.toLowerCase() === "y") {
@@ -15,18 +17,56 @@ if (userAccept.toLowerCase() === "y") {
   }
 }
 
-let brendanAge = prompt("How old am I?");
-
-switch (brendanAge) {
-  case 25:
-    alert("You got it!");
+// Question 2 (Switch)
+let brendanState = prompt("I'm from Pennsylvania(Y/N)");
+switch (brendanState.toLowerCase()) {
+  case "y":
+    // console.log('Nope, not from PA');
+    alert("Nope, not from PA");
     break;
-  case 24:
-    alert("Almost had it!");
-    break;
-  case 25:
-    alert("Ooooo you were close!");
+  case "n":
+    // console.log('You are correct!');
+    alert("You are correct!");
     break;
   default:
-    alert("Nope you are wrong");
+    alert(
+      "That's not even a choice, why would you choose that? Learn to follow directions."
+    );
+    break;
+}
+
+// Question 3 (If)
+let brendanHome = prompt("Do I live in Fort Worth Texas currently? (Y/N)");
+
+if (brendanHome.toLowerCase() === "y") {
+  // console.log('Ding ding ding! you're on a roll!');
+  alert("Ding ding ding! you're on a roll!");
+} else {
+  // console.log('You are wrong!);
+  alert("You are wrong!");
+}
+
+// Question 4 (Switch)
+let brendanHobby = prompt("Working out in the gym is one of my hobbies? (Y/N)");
+
+switch (brendanHobby.toLowerCase()) {
+  case "y":
+    // console.log('Yep love getting swole');
+    alert("Yep love getting swole!");
+    break;
+  case "n":
+    // console.log("Well that's just not quite right there partner");
+    alert("Well that's just not quite right there partner");
+    break;
+}
+
+// Question 5 (If)
+let brendanPSM = prompt("I am a certified Professional Scrum Master 1? (Y/N)");
+if (brendanPSM.toLowerCase() === "y") {
+  // console.log(`Yep you got it thanks for taking my quiz ${userName}`);
+  alert(`Yep you got it thanks for taking my quiz ${userName} here's my site`);
+} else {
+  // console.log('You got this one wrong');
+  alert("You got this one wrong");
+  alert("Anyway here is my site!");
 }
