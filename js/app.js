2114a11ac2cb917c208e3c25f6531fa2e5aa58d2
Brendan Huddleston <brendanhuddleston18@gmail.com>
2023-10-25 20:32:24 -0500

    Fixed questions 6 and 7 to where they didn't accept the correct answers

--- js/app.js
+++ js/app.js
@@ -5,13 +5,24 @@ let userName = prompt('Hey what is your name?');
 console.log(userName);
 alert(`Welcome ${userName}, to my work in progress 'About Me' site!`);
 
+// let questionOne;
+// let questionTwo;
+// let questionThree;
+// let questionFour;
+// let questionFive;
+// let questionSix;
+// let questionSeven;
+// let userQuiz = [questionOne,questionTwo,questionThree,questionFour,questionFive,questionSix,questionSeven];
 //Question 1 (If condition)
 let userAccept = prompt('We are gonna take a quick quiz is that okay? (Y/N)');
 let letter = userAccept.substring(0, 1).toLowerCase();
 console.log(letter);
+let correct = 0;
 if (letter === 'y') {
   console.log('Strap in here we go!');
   alert('Strap in here we go!');
+  correct++;
+  console.log(correct);
 } else {
   // console.log('Placeholder because my while loop will not work in lighthouse');
   while (userAccept.toLowerCase() !== 'y') {
@@ -30,6 +41,8 @@ case 'y':
 case 'n':
   // console.log('You are correct!');
   alert('You are correct!');
+  correct++;
+  console.log(correct);
   break;
 default:
   alert(
@@ -44,6 +57,8 @@ let brendanHome = prompt('Do I live in Fort Worth Texas currently? (Y/N)');
 if (brendanHome.toLowerCase() === 'y') {
   // console.log('Ding ding ding! you're on a roll!');
   alert("Ding ding ding! you're on a roll!");
+  correct++;
+  console.log(correct);
 } else {
   // console.log('You are wrong!);
   alert('You are wrong!');
@@ -56,6 +71,8 @@ switch (brendanHobby.toLowerCase()) {
 case 'y':
   // console.log('Yep love getting swole');
   alert('Yep love getting swole!');
+  correct++;
+  console.log(correct);
   break;
 case 'n':
   // console.log("Well that's just not quite right there partner");
@@ -67,11 +84,12 @@ case 'n':
 let brendanPSM = prompt('I am a certified Professional Scrum Master 1? (Y/N)');
 if (brendanPSM.toLowerCase() === 'y') {
   // console.log(`Yep you got it thanks for taking my quiz ${userName}`);
-  alert(`Yep you got it thanks for taking my quiz ${userName} here's my site`);
+  alert(`Yep you got it ${userName}`);
+  correct++;
+  console.log(correct);
 } else {
   // console.log('You got this one wrong');
   alert('You got this one wrong');
-  alert('Anyway here is my site!');
 }
 
 // Question 6 (Only four chances)
@@ -84,13 +102,17 @@ for(let userChance = 1; userChance < 5; userChance++){
     break;
   }
   console.log(n);
-  if(userGuessAge === 25){
+  if(Number(userGuessAge) === 25){
     alert(`Good Job ${userName}`);
+    correct++;
+    console.log(correct);
     break;
   } else if(userGuessAge > 25){
     userGuessAge = prompt(`Nope you're too high try again, you're on your ${userChance} attempt`);
   } else if(userGuessAge < 25){
     userGuessAge = prompt(`Nope you're too low try again, you're on your ${userChance} attempt`);
+  } else {
+    userGuessAge = prompt(`Nope, that's not even a number, you're on your ${userChance} attempt`);
   }
   n++;
 }
@@ -99,20 +121,37 @@ for(let userChance = 1; userChance < 5; userChance++){
 let statesLived = prompt('Which of these states have I lived in?: Colorado, Tennessee, Alabama, Texas, California');
 let correctStates = ['colorado','texas'];
 let limit = 1;
-while(statesLived.toLowerCase() !== correctStates[0] || statesLived !== correctStates[1]){
+while(statesLived.toLowerCase() !== correctStates[0] || statesLived.toLowerCase() !== correctStates[1]){
+  if(statesLived.toLowerCase() == correctStates[0]){
+    alert('You got it, the other answer was Texas!');
+    correct++;
+    console.log(correct);
+    break;
+  } else if (statesLived.toLowerCase() == correctStates[1]){
+    alert('You got it, the other answer was Colorado!');
+    correct++;
+    console.log(correct);
+    break;
+  }
   statesLived = prompt(`Sorry, that is incorrect try again: Colorado, Tennessee, Alabama, Texas, California, you are on attempt ${limit}/6`);
   if(limit >= 6){
     alert('The two correct answers were Texas or Colorado!');
     break;
   }
-  if(statesLived.toLowerCase() == correctStates[0]){
-    alert('You got it, the other answer was Texas!');
-    break;
-  } else if (statesLived.toLowerCase() == correctStates[1]){
-    alert('You got it, the other answer was Colorado!');
-    break;
-  }
   limit++;
+  // if(statesLived.toLowerCase() == correctStates[0]){
+  //   alert('You got it, the other answer was Texas!');
+  //   correct++;
+  //   console.log(correct);
+  //   break;
+  // } else if (statesLived.toLowerCase() == correctStates[1]){
+  //   alert('You got it, the other answer was Colorado!');
+  //   correct++;
+  //   console.log(correct);
+  //   break;
 }
+
+
+alert(`Thanks for taking my quiz! you got a ${correct}/7!`);
 
 
